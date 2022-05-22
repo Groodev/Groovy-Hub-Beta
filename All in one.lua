81f72ecb3146b5453c9b35d212cef7ab0de7bb69
Groodev <104547527+Groodev@users.noreply.github.com>
2022-05-22 10:58:40 +0700

    Create All in one.lua

---
+++ All in one.lua
@@ -0,0 +1,15 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Groovy Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local Tab = Window:MakeTab({
+
+Name = "Tab 1",	Icon = "rbxassetid://4483345998",
+
+PremiumOnly = false
+
+
+
+
+
+
+
